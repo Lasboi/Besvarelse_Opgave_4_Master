792b77acd41a713390325306067b634d776f16a8
LasBoi <Azlj1689gk>
2019-09-23 16:19:01 +0200

    Oprettet første test case til opgave 5_3

--- test/Opgave_5_3/ConverterTest.java
+++ test/Opgave_5_3/ConverterTest.java
@@ -4,4 +4,17 @@ import junit.framework.TestCase;
 
 public class ConverterTest extends TestCase {
 
+    //Test case - Loop 50 gange
+    //Input: i = 0
+    //Forventet output: i = 50
+
+    public void testLoop() {
+        int i = 0;
+        while (i < 50) {
+            //System.out.println(i);
+            i++;
+        }
+        assertEquals(50, i);
+    }
+
 }
