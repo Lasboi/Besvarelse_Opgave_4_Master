c9587b33e21220895ba5dd21c3e477a4b06527ad
LasBoi <Azlj1689gk>
2019-09-23 14:38:47 +0200

    Begyndt at udvikle på CrazyMath klassen

--- src/Opgave_5_2/CrazyMath.java
+++ src/Opgave_5_2/CrazyMath.java
@@ -1,4 +1,17 @@
 package Opgave_5_2;
 
+import java.util.Scanner;
+
 public class CrazyMath {
+
+    static final int numberOfQuestions = 10;
+    static int correctCount = 0;
+    static int count = 0;
+    static long startTime = System.currentTimeMillis();
+    static String output = " ";
+
+    static void Setup() {
+        Scanner scanner = new Scanner(System.in);
+    }
+
 }
