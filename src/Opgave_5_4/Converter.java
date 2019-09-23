af60961b8095ab9fcbe8925eef6f4ce1c7b06321
LasBoi <Azlj1689gk>
2019-09-23 16:55:31 +0200

    Fuldført opgaven samt skrevet kommentarer til koden

--- src/Opgave_5_4/Converter.java
+++ src/Opgave_5_4/Converter.java
@@ -1,4 +1,26 @@
 package Opgave_5_4;
 
 public class Converter {
+
+    //Vores setup metoden som køre hele programmet
+    static void Setup() {
+        int inches = 1;
+        double centimeter;
+
+        System.out.println("Inches      Centimeter");
+
+        while (inches != 11) { //Vi kører vores loop 10 gange (1-10)
+            centimeter = ((double) inches) * 2.54; //Vi udregner fra inches til cm
+            System.out.println(" " + inches + "     |    " + centimeter);
+            inches++;
+        }
+    }
+
+    //Made by metoden fordi det er cool
+    static void madeBy() {
+        System.out.println("");
+        System.out.println("******************************");
+        System.out.println("******* Kodet af Lasse *******");
+        System.out.println("******************************");
+    }
 }
--- src/Opgave_5_4/Main.java
+++ src/Opgave_5_4/Main.java
@@ -1,4 +1,8 @@
 package Opgave_5_4;
 
 public class Main {
+    public static void main(String[] args) {
+        Converter.Setup(); //Vi kalder vores setup
+        Converter.madeBy();
+    }
 }
